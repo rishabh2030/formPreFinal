f7e2463d4ffd6bb8555aaf8e1a0cab9981b312be
rishabh2030 <rishabh.sharma1@ssipmt.com>
2022-10-11 05:47:22 +0530

    validation in from

---
+++ agian/Pages/Index.cshtml.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using System.Net.Mail;
+using System.Net;
+
+namespace agian.Pages
+{
+    public class IndexModel : PageModel
+    {
+        
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+            string FN = Request.Form["FirstName"];
+            string LN = Request.Form["LastName"];
+            string email = Request.Form["Email"];
+            string phoneNo = Request.Form["PhoneNo"];
+            string DOB = Request.Form["DOB"];
+            string gender = Request.Form["Gender"];
+            string city = Request.Form["City"];
+            string pinCode = Request.Form["Pincode"];
+            string curr_Add = Request.Form["Address"];
+            string pre_Add = Request.Form["AddressP"];
+            string institute = Request.Form["Institute"];
+            string year = Request.Form["Year"];
+            string type = Request.Form["Field"];
+            string duration = Request.Form["Duration"];
+            string skills = Request.Form["Skills"];
+            string major = Request.Form["Major"];
+            string qualification = Request.Form["Qualification"];
+            string AOI = Request.Form["Interest"];
+            string expr = Request.Form["Experience"];
+            string PWP = Request.Form["WorkProfile"];
+            string POWE = Request.Form["WorkExperience"];
+            string CTC = Request.Form["CTC"];
+
+            string connectionString = "Data Source=DESKTOP-PV6H4SA\\SQLEXPRESS;Initial Catalog=BI_form;Integrated Security=True;TrustServerCertificate=True";
+
+
+            string sqlQuery =
+                " INSERT INTO Information(FirstName,LastName,email,phoneNo,city,gender,pincode,c_address,p_address,Instituet_Name,year_of_com,type_of_training,duration,skills,Major,qualification,AreaOfIntrest,expr,past_profile,past_exp,current_CTC)"
++
+"VALUES (" +
+" ' " + FN + " ' " +","
++
+" ' " + LN + " ' " + ","
++
+" ' " + email + " ' " + ","
++
+" ' " + phoneNo + " ' " + ","
++
+" ' " + city + " ' " + ","
++
+" ' " + gender + " ' " + ","
++
+" ' " + pinCode + " ' " + ","
++
+" ' " + curr_Add + " ' " + ","
++
+" ' " + pre_Add + " ' " + ","
++
+" ' " + institute + " ' " + ","
++
+" ' " + year + " ' " + ","
++
+" ' " + type + " ' " + ","
++
+" ' " + duration + " ' " + ","
++
+" ' " + skills + " ' " + ","
++
+" ' " + major + " ' " + ","
++
+" ' " + qualification + " ' " + ","
++
+" ' " + AOI + " ' " + ","
++
+" ' " + expr + " ' " + ","
++
+" ' " + PWP + " ' " + ","
++
+" ' " + POWE + " ' " + ","
++
+" ' " + CTC + " ' " + " ) ";
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand sc = new SqlCommand(sqlQuery, con);
+            sc.ExecuteNonQuery();
+            con.Close();
+            string fullName = FN + " " + LN;
+            string Message = "Thank you for Showing your interest" + FN + " " + LN;
+            SendMail(fullName, email, Message);
+
+        }
+        public bool SendMail(string name, string email, string Message)
+        {
+            MailMessage message = new MailMessage();
+            SmtpClient smtpClient = new SmtpClient();
+            message.From = new MailAddress("noreply@zebit.tech");
+            message.To.Add("rishabh.sharma13@outlook.com");
+            message.Subject = "Test email";
+            message.IsBodyHtml = true;
+            message.Body = "<h1>" + name + "</h1>";
+
+            smtpClient.Port = 587;
+            smtpClient.Host = "smtp.office365.com";
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential("rishabh.sharma13@outlook.com", "R8770300675s");
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            
+            smtpClient.Send(message);
+            return true;
+
+
+        }
+    }
+}
---
+++ agian/wwwroot/js/site.js
@@ -0,0 +1,328 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+
+
+
+
+function openDivTraining() {
+    document.getElementById('divTraining').style.display = 'block';
+    document.getElementById('divInternship').style.display = 'none';
+    document.getElementById('divJob').style.display = 'none';
+}
+
+function openDivInternship() {
+    document.getElementById('divInternship').style.display = 'block';
+    document.getElementById('divTraining').style.display = 'none';
+    document.getElementById('divJob').style.display = 'none';
+}
+
+
+function openDivJob() {
+    document.getElementById('divJob').style.display = 'block';
+    document.getElementById('divInternship').style.display = 'none';
+    document.getElementById('divTraining').style.display = 'none';
+}
+function alertshow() {
+    if ($("#name").val() == "") {
+        $('#name').removeClass('bordernone');
+        $('#name').addClass('textbox');
+    }
+    else {
+       $('#name').removeClass('textbox');
+    }
+    //alert("We will let you know thank you for showing your intrest");
+}
+
+
+// to get city name from pin code
+
+function check() {
+    var pincode = document.getElementById("pincode");
+    var head = document.getElementById("city");
+
+    console.log(pincode.value);
+    $.ajax({
+        type: "GET",
+        url: "https://api.postalpincode.in/pincode/" + pincode.value,
+        success: function (reponse) {
+            console.log(reponse[0].PostOffice[0]);
+            head.value = reponse[0].PostOffice[0].Region;
+        },
+        error: function (data) {
+            console.log(data);
+        }
+
+    })
+}
+// to make current address and parmanet address same
+function setBillingAddress() {
+    if ($("#homepostalcheck").is(":checked")) {
+        $('#addressp').val($('#address').val());
+        $('#addressp').attr('disabled', 'disabled');
+    } else {
+        $('#addressp').removeAttr('disabled');
+        $('#addressp').val(" ");
+    }
+}
+
+$('#homepostalcheck').click(function () {
+    setBillingAddress();
+})
+
+// 
+function check() {
+    var pincode = document.getElementById("pincode");
+    var head = document.getElementById("city");
+
+    console.log(pincode.value);
+    $.ajax({
+        type: "GET",
+        url: "https://api.postalpincode.in/pincode/" + pincode.value,
+        success: function (reponse) {
+            console.log(reponse[0].PostOffice[0]);
+            head.value = reponse[0].PostOffice[0].Region;
+        },
+        error: function (data) {
+            console.log(data);
+        }
+
+    })
+}
+//
+
+// validate Address
+
+const AddressChecker = (Number) => {
+    return Number.match(
+        /^([^\p{N}\p{S}\p{C}\p{P}]{2,200})$/
+    );
+};
+
+const validateAdress = () => {
+
+    const Address = $('#address').val();
+
+
+    if (AddressChecker(Address)) {
+
+        $('#address').css({
+            'background-color': 'white',
+        });
+        document.getElementById("submit").disabled = false;
+  
+    } else {
+
+        $('#address').css({
+            'background-color': '#FFCCCB',
+        });
+        document.getElementById("submit").disabled = true;
+    }
+    return false;
+}
+
+$('#address').on('input', validateAdress);
+
+// validate email
+
+const emailAddressChecker = (email) => {
+    return email.match(
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    );
+};
+
+const validateEmailAdress = () => {
+
+    const email = $('#email').val();
+
+
+    if (emailAddressChecker(email)) {
+
+        $('#email').css({
+            'background-color': 'white',
+        });
+        
+    } else {
+
+        $('#email').css({
+            'background-color': '#FFCCCB',
+        });
+        document.getElementById("submit").disabled = true;
+    }
+    return false;
+}
+
+$('#email').on('input', validateEmailAdress);
+
+// validate FN And LS
+
+
+const namechecker = (name) => {
+    return name.match(
+        /^([^\p{N}\p{S}\p{C}\p{P}]{2,20})$/
+    );
+};
+
+const validateNameFN = () => {
+
+    const name = $('#FirstName').val();
+
+
+    if (namechecker(name)) {
+
+        $('#FirstName').css({
+            'background-color': 'white',
+        });
+        
+    } else {
+
+        $('#FirstName').css({
+            'background-color': '#FFCCCB',
+        });
+        document.getElementById("submit").disabled = true;
+    }
+    return false;
+    
+}
+
+$('#FirstName').on('input', validateNameFN);
+// LN
+const namechecker1 = (name) => {
+    return name.match(
+        /^([^\p{N}\p{S}\p{C}\p{P}]{2,20})$/
+    );
+};
+
+const validateNameLN = () => {
+
+    const name = $('#LastName').val();
+
+
+    if (namechecker1(name)) {
+
+        $('#LastName').css({
+            'background-color': 'white',
+        });
+      
+    } else {
+
+        $('#LastName').css({
+            'background-color': '#FFCCCB',
+        });
+        document.getElementById("submit").disabled = true;
+    }
+    return false;
+}
+
+$('#LastName').on('input', validateNameLN);
+
+
+// validate number
+
+const numberChecker = (number) => {
+    return number.match(
+
+        /^[0-9]{10}$/
+    );
+};
+
+const validatenumberPhone = () => {
+
+    const number = $('#number').val();
+
+
+    if (numberChecker(number)) {
+
+        $('#number').css({
+            'background-color': 'white',
+        });
+        
+    } else {
+
+        $('#number').css({
+            'background-color': '#FFCCCB',
+        });
+        document.getElementById("submit").disabled = true;
+    }
+    return false;
+}
+
+$('#number').on('input', validatenumberPhone);
+
+// validate pinCode
+
+const pinNumberChecker = (number) => {
+    return number.match(
+
+        /^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$/
+    );
+};
+
+const validatenumberPin = () => {
+
+    const pinNumber = $('#pincode').val();
+
+
+    if (pinNumberChecker(pinNumber)) {
+
+        $('#pincode').css({
+            'background-color': 'white',
+        });
+        
+    } else {
+
+        $('#pincode').css({
+            'background-color': '#FFCCCB',
+        });
+        document.getElementById("submit").disabled = true;
+    }
+    return false;
+}
+
+$('#pincode').on('input', validatenumberPin);
+
+// validate college name
+
+const collegeName = (name) => {
+    return name.match(
+
+        /^([^\p{N}\p{S}\p{C}\p{P}]{2,200})$/
+    );
+};
+
+const validateCollegeName = () => {
+
+    const name = $('#institue').val();
+
+
+    if (collegeName(name)) {
+
+        $('#institue').css({
+            'background-color': 'white',
+            
+        });
+        
+        document.getElementById("submit").disabled = false;
+        
+    } else {
+
+        $('#institue').css({
+            'background-color': '#FFCCCB',
+            
+
+        });
+        document.getElementById("submit").disabled = true;
+        
+   
+    }
+    return false;
+}
+
+$('#institue').on('input', validateCollegeName);
+//document.getElementById("submit").disabled = true;
+
+    
+
